5a11cbfa342265a290694d244bbe4d15420c4826
shreyshrey616 <shreyas.srinivasan@gmail.com>
2014-08-18 15:30:28 -0700

    started skeleton of bugServer

--- Operations GUI/bugServer.py
+++ Operations GUI/bugServer.py
@@ -7,12 +7,38 @@ context = zmq.Context()
 dbSocket = context.socket(zmq.REP)
 dbSocket.connect("tcp://localhost:5108")
 
+setMistiConfig_state = 0 #0 if MISTI_CONFIG not set, 1 if set
+createDirs_state = 0 #0 if directories to be saved into not set up, 1 if set
+ladybug_state = 0 #0 if ladybug not started, 1 if only port started, 2 if port and starboard started
+lidar_state = 0 #0 if lidar not started, 1 if only port started, 2 if port and starboard started
+arduino_state = 0 #0 if startArduino not started, 1 if started
+weather_state = 0 #0 if weather not started, 1 if started
+GPS_state = 0 #0 if Novatel GPS not started, 1 if started
+system_state = 0 #0 if ladybug, lidar, arduino all not started, 1 if ladybug, lidar, arduino, weather, GPS all started
+				 #need to check if we also need to turn off weather and GPS
+
+
 
 while True:
 
 	dbCommand = dbSocket.recv()
 
-	if dbCommand == str:
+	if dbCommand == sta:
+		if system_state == 0:
+			#subprocess.Popen("setMistiConfig", cwd=r"path\\to\\setMistiConfig", stdout = subprocess.PIPE)
+			#verify setMistiConfig
+			#subprocess.Popen("createDirs.bat", cwd=r"C:\\path\\to\\createDirs.bat", stdout = subprocess.PIPE)
+			#verify createDirs
+			while lidar_state = 0:
+					#run startVelodynePort and verify
+					lidar_state = 1 #if verification is true
+
+
+			#starboard lidar and verification
+			#
+
+
+
 
 	#based on what the start script returns may want to use subprocess.callback() and use resulting info for data verification
 		subprocess.call(#insert bug start script name here#)
@@ -20,7 +46,7 @@ while True:
 	if dbCommand == stp:
 		subprocess.call(#insert bug stop script name here#)
 	
-	if dbCommand == save:
+	if dbCommand == save: #data offload
 		subprocess.call(#insert bug save script name here#)
 		
 	#function to display live data
