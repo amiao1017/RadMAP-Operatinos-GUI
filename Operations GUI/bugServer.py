7663870457fd285520776f4d62657f61e37ccf2e
Shreyas Srinivasan <Shreyas@shreyass-mbp-2.dhcp.lbnl.us>
2014-08-19 11:30:20 -0700

    bug skeleton

--- Operations GUI/bugServer.py
+++ Operations GUI/bugServer.py
@@ -25,12 +25,12 @@ while True:
 
 	if dbCommand == sta:
 		if system_state == 0:
-            while weather_state = 0:
+            while weather_state == 0:
                 #start weather app
                 #verify weather app
                 #if verification true
                     weather_state = 1
-            while GPS_state = 0:
+            while GPS_state == 0:
                 #start GPS
                 #verify GPS started
                 #if verification true
@@ -41,37 +41,77 @@ while True:
 			#verify setMistiConfig
 			#subprocess.Popen("createDirs.bat", cwd=r"C:\\path\\to\\createDirs.bat", stdout = subprocess.PIPE)
 			#verify createDirs
-			while lidar_state = 0:
+			while lidar_state == 0:
                 #run startVelodynePort and verify
                 #if verification case is true
                     lidar_state = 1 #if verification is true
-            while lidar_state = 1:
+            while lidar_state == 1:
                 #run startVelodyneStarboard and verify
                 #if verification case is true
                     lidar_state = 2
-            while ladybug_state = 0:
+            while ladybug_state == 0:
                 #run startLadybugPort and verify
                 #if verification true
                     ladybug_state = 1
-            while ladybug_state = 1:
+            while ladybug_state == 1:
                 #run startLadybugStarboad and verify
                 #if verification true
                     ladybug_state = 2
-            while arduino_state = 0:
+            while arduino_state == 0:
                 #run startArduino and verify
                 #if verification true
                     arduino_state = 1
-            if (weather_state == 1) and (GPS_state == 1) and (lidar_state == 2) and (ladybug_state == 2) and (arduino_state == 2):
+            if (weather_state == 1) and (GPS_state == 1) and (lidar_state == 2) and (ladybug_state == 2) and (arduino_state == 1):
                 system_state = 1
-            
 
-
-	#based on what the start script returns may want to use subprocess.callback() and use resulting info for data verification
-		subprocess.call(#insert bug start script name here#)
-		
 	if dbCommand == stp:
-		subprocess.call(#insert bug stop script name here#)
-	
+        if system_state == 1:
+            while weather_state == 1:
+                #stop weather app
+                #verify stop
+                #if verification true
+                    weather_state = 0
+            while GPS_state == 1:
+                #stop GPS
+                #verify GPS stopped
+                #if verification true
+                    GPS_state = 1
+            while lidar_state == 2: #both port and starboard lidar on
+                #stop starboard lidar
+                #verify stoppage
+                #if stopped
+                    lidar_state = 1
+            while lidar_state == 1: #port lidar on
+                #stop port lidar
+                #verify stoppage
+                #if stopped
+                    lidar_state = 0
+            while ladybug_state == 2:
+                #stop starboard ladybug
+                #verify stoppage
+                #if stopped
+                    ladybug_state = 1
+            while ladybug_state == 1:
+                #stop port ladybug
+                #verify stoppage
+                #if stopped
+                    ladybug_state = 0
+            while arduino_state == 1:
+                #stop arduino
+                #verify stoppage
+                #if stopped
+                    arduino_state = 0
+            if (weather_state == 0) and (GPS_state == 0) and (lidar_state == 0) and (ladybug_state == 0) and (arduino_state == 0):
+                system_state = 0
+    
+    if system_state == 1: #the start message has been received and all the sensors are running
+        #weather verification
+        #GPS verification
+        #ladybug/lidar/arduino verification
+        
+        
+                
+
 	if dbCommand == save: #data offload
 		subprocess.call(#insert bug save script name here#)
 		
