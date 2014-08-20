a9ca5b0fdcd1bf8a59142b1c2726557c64b4fa38
Shreyas Srinivasan <Shreyas@shreyass-mbp-2.dhcp.lbnl.us>
2014-08-19 17:08:51 -0700

    rewrote if statements and logic

--- Operations GUI/bugServer.py
+++ Operations GUI/bugServer.py
@@ -7,112 +7,121 @@ context = zmq.Context()
 dbSocket = context.socket(zmq.REP)
 dbSocket.connect("tcp://localhost:5108")
 
-setMistiConfig_state = 0 #0 if MISTI_CONFIG not set, 1 if set
 createDirs_state = 0 #0 if directories to be saved into not set up, 1 if set
+weather_state = 0 #0 if weather not started, 1 if started
+GPS_state = 0 #0 if Novatel GPS not started, 1 if started
 ladybug_state = 0 #0 if ladybug not started, 1 if only port started, 2 if port and starboard started
 lidar_state = 0 #0 if lidar not started, 1 if only port started, 2 if port and starboard started
 arduino_state = 0 #0 if startArduino not started, 1 if started
-weather_state = 0 #0 if weather not started, 1 if started
-GPS_state = 0 #0 if Novatel GPS not started, 1 if started
-system_state = 0 #0 if ladybug, lidar, arduino all not started, 1 if ladybug, lidar, arduino, weather, GPS all started
-				 #need to check if we also need to turn off weather and GPS
+system_state = 0 #0 if weather
+                 #1 if ladybug, lidar, and arduino not started, but system ready to start capturing data
+                 #2 if all sensors started and capturing data
 
 
 
 while True:
-
-	dbCommand = dbSocket.recv()
-
-	if dbCommand == sta:
-		if system_state == 0:
-            while weather_state == 0:
-                #start weather app
-                #verify weather app
-                #if verification true
-                    weather_state = 1
-            while GPS_state == 0:
-                #start GPS
-                #verify GPS started
-                #if verification true
-                    GPS_state = 1
-            
-            
-			#subprocess.Popen("setMistiConfig", cwd=r"path\\to\\setMistiConfig", stdout = subprocess.PIPE)
-			#verify setMistiConfig
-			#subprocess.Popen("createDirs.bat", cwd=r"C:\\path\\to\\createDirs.bat", stdout = subprocess.PIPE)
-			#verify createDirs
-			while lidar_state == 0:
-                #run startVelodynePort and verify
-                #if verification case is true
-                    lidar_state = 1 #if verification is true
-            while lidar_state == 1:
-                #run startVelodyneStarboard and verify
-                #if verification case is true
-                    lidar_state = 2
-            while ladybug_state == 0:
-                #run startLadybugPort and verify
-                #if verification true
-                    ladybug_state = 1
-            while ladybug_state == 1:
-                #run startLadybugStarboad and verify
-                #if verification true
-                    ladybug_state = 2
-            while arduino_state == 0:
-                #run startArduino and verify
-                #if verification true
-                    arduino_state = 1
-            if (weather_state == 1) and (GPS_state == 1) and (lidar_state == 2) and (ladybug_state == 2) and (arduino_state == 1):
-                system_state = 1
-
-	if dbCommand == stp:
-        if system_state == 1:
-            while weather_state == 1:
-                #stop weather app
-                #verify stop
-                #if verification true
-                    weather_state = 0
-            while GPS_state == 1:
-                #stop GPS
-                #verify GPS stopped
-                #if verification true
-                    GPS_state = 1
-            while lidar_state == 2: #both port and starboard lidar on
-                #stop starboard lidar
-                #verify stoppage
-                #if stopped
-                    lidar_state = 1
-            while lidar_state == 1: #port lidar on
-                #stop port lidar
-                #verify stoppage
-                #if stopped
-                    lidar_state = 0
-            while ladybug_state == 2:
-                #stop starboard ladybug
-                #verify stoppage
-                #if stopped
-                    ladybug_state = 1
-            while ladybug_state == 1:
-                #stop port ladybug
-                #verify stoppage
-                #if stopped
-                    ladybug_state = 0
-            while arduino_state == 1:
-                #stop arduino
-                #verify stoppage
-                #if stopped
-                    arduino_state = 0
-            if (weather_state == 0) and (GPS_state == 0) and (lidar_state == 0) and (ladybug_state == 0) and (arduino_state == 0):
-                system_state = 0
+    #Verification if the different sensors are running
+    if weather_state == 0:
+    #verify weather app
+    if: #verification true
+    weather_state = 1
+    else: #start weather app
+    #verify weather app
+    #if verification true
+    weather_state = 1
+    if GPS_state == 0:
+    #verify GPS started
+    if: #verification true
+    GPS_state = 1
+    else: #start GPS
+    #verify GPS started
+    #if verification true
+    GPS_state = 1
     
-    if system_state == 1: #the start message has been received and all the sensors are running
-        #weather verification
-        #GPS verification
-        #ladybug/lidar/arduino verification
-        
-        
-                
+    
+    startCapture = subprocess.Popen("setMistiConfig", cwd=r"E:", stdin = subprocess.PIPE, stdout = subprocess.PIPE)
+    if createDirs_state == 0:
+    createDirs = subprocess.Popen("createDirs.bat", cwd=r"E:", stdout = subprocess.PIPE) #set up the directories
+    #verify createDirs
+    createDirs_state = 1
+    if createDirs_state == 1 and system_state == 1:
+    #verify data for Lidars, Ladybugs, and Arduino
+    #socket send data/string/verification message
 
-	if dbCommand == save: #data offload
-		subprocess.call(#insert bug save script name here#)
-		
+    if dbSocket.poll(milliseconds) != 0:
+        dbCommand = dbSocket.recv()
+
+        if dbCommand == sta:
+            if system_state == 0: #effectively runs the startCapture.bat file
+                if weather_state == 0:
+                    #verify weather app
+                    if: #verification true
+                    weather_state = 1
+                    else: #start weather app
+                        #verify weather app
+                        #if verification true
+                        weather_state = 1
+                if GPS_state == 0:
+                    #verify GPS started
+                    if: #verification true
+                    GPS_state = 1
+                    else: #start GPS
+                        #verify GPS started
+                        #if verification true
+                        GPS_state = 1
+                if createDirs_state == 1:
+                    #run startVelodynePort and verify
+                    #run startVelodyneStarboard and verify
+                    #run startLadybugPort and verify
+                    #run startLadybugStarboad and verify
+                    #run startArduino and verify
+                    #if all verification true
+                        system_state = 1
+                
+        if dbCommand == stp:
+            if system_state == 1:
+#                if weather_state == 1:
+#                    #stop weather app
+#                    #verify stop
+#                    #if verification true
+#                        weather_state = 0
+#                if GPS_state == 1:
+#                    #stop GPS
+#                    #verify GPS stopped
+#                    #if verification true
+#                        GPS_state = 1
+                if lidar_state == 2: #both port and starboard lidar on
+                    #stop starboard lidar
+                    #verify stoppage
+                    #if stopped
+                        lidar_state = 1
+                if lidar_state == 1: #port lidar on
+                    #stop port lidar
+                    #verify stoppage
+                    #if stopped
+                        lidar_state = 0
+                if ladybug_state == 2:
+                    #stop starboard ladybug
+                    #verify stoppage
+                    #if stopped
+                        ladybug_state = 1
+                if ladybug_state == 1:
+                    #stop port ladybug
+                    #verify stoppage
+                    #if stopped
+                        ladybug_state = 0
+                if arduino_state == 1:
+                    #stop arduino
+                    #verify stoppage
+                    #if stopped
+                        arduino_state = 0
+                if (weather_state == 0) and (GPS_state == 0) and (lidar_state == 0) and (ladybug_state == 0) and (arduino_state == 0):
+                    system_state = 0
+            
+                    
+
+        if dbCommand == save: #data offload
+            subprocess.call(#insert bug save script name here#)
+
+
 	#function to display live data
