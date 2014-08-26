0f3911cb235e63c9168384bc2ac22304cf789879
shreyshrey616 <shreyas.srinivasan@gmail.com>
2014-08-26 13:15:19 -0700

    finished verification for lidars, ladybugs and arduino

--- Operations GUI/bugServer.py
+++ Operations GUI/bugServer.py
@@ -19,15 +19,16 @@ velodyneStarboardStarted = False
 ladybugPortStarted = False
 ladybugStarboardStarted = False
 arduinoStarted = False
-weatherStarted = False
-gpsStarted = False
 
 
+processes = [] #list of all the processes running
+iterations = 0 #number of iterations through loop. used to index list of processes for verification 
 #States
 createDirs_state = 0 #0 if directories to be saved into not set up, 1 if set. if 1, ladybugs, lidar and arduino can be started
 system_state = 0 #0 if sta command not received
-                 #1 if sta command received and all sensors started collecting data
-
+                 #1 if sta command received and Lidar/Ladybug/Arduino collecting data
+weather_state = 0
+GPS_state = 0
 
 
 while True:
@@ -49,45 +50,70 @@ while True:
             #if verification true
             GPS_state = 1
     
-    
-    
+    #Verification of Lidar, Ladybug, Arduino
+    if len(processes):
+        line = processes[iterations % len(processes)].stdout.readline() #NEED TIMEOUT TO PREVENT BLOCKING
+        if (line.find("time_len: ") != -1): #arduino verification
+            dbSocket.send("Arduino - " + line[line.find("time_len: "): ]
+            if !arduinoStarted:
+                arduinoStarted = True
+        if (line.find("Images: ") != -1): #Ladybug Verification
+            if (line.find("11291123") != -1): #port ladybug
+                dbSocket.send("Port Ladybug - " + line[line.find("Images: "): line.find(" | MB")])
+                if !velodynePortStarted:
+                    velodynePortStarted = True
+            if (line.find("11260640") != -1): #starboard ladybug
+                dbSocket.send("Starboard Ladybug - " + line[line.find("Images: "): line.find(" | MB")])
+                if !velodyneStarboardStarted:
+                    velodyneStarboardStarted = True
+        if ((line.find("Packets captured: ") != -1):
+            if (line.find("711024488") != -1): #starboard lidar
+                dbSocket.send("Starboard Lidar - " + line[line.find("Packets captured: "): ])
+                if !ladybugStarboardStarted:
+                    ladybugStarboardStarted = True
+            if (line.find("711024572") != -1): #port lidar
+                dbSocket.send("Port Lidar - " + line[line.find("Packets captured: "): ])
+                if !ladybugPortStarted:
+                    ladybugPortStarted = True
+        iterations += 1
+        if velodynePortStarted and velodyneStarboardStarted and ladybugPortStarted and ladybugStarboardStarted and arduinoStarted: #if all verification true
+            system_state = 1
 
-    if system_state == 1:
-    #verify data for Lidars, Ladybugs, and Arduino
-    dbSocket.send("Port Lidar -" + lidarVerificationString(startVelodynePort), NOBLOCK)
-    dbSocket.send("Starboard Lidar -" + lidarVerificationString(startVelodyneStarboard), NOBLOCK)
-    dbSocket.send("Port Ladybug -" + ladybugVerificationString(startLadybugPort), NOBLOCK)
-    dbSocket.send("Starboard Ladybug -" + ladybugVerificationString(startLadybugStarboad), NOBLOCK)
-    dbSocket.send("Arduino -" + arduinoVerificationString(startArduino), NOBLOCK)
-    #socket send data/string/verification message
 
     if dbSocket.poll(milliseconds) != 0:
         dbCommand = dbSocket.recv()
 
-        if dbCommand == sta: #start all sensors on bug
-            weatherProcess = subprocess.Popen("weatherStartScript", cwd=r"Path\\to\\weatherStartScript", stdout = subprocess.PIPE) #start weather data
-            GPSProcess = subprocess.Popen("GPSStartScript", cwd=r"Path\\to\\GPSStartScript", stdout = subprocess.PIPE)
-            if createDirs_state == 0:
-                createDirs = subprocess.Popen("createDirsGUI.bat", cwd=r"Path\\to\\createDirsGui", stdout = subprocess.PIPE) #set up the directories
-                #verify createDirs
-                date = datetime.now()
-                checkFilename = glob.glob(filename)
-                if checkFilename[0] == filename:
-                    createDirs_state = 1
+        for cmd in dbCommand:
+            if cmd == "STA":
+                if createDirs_state == 0:
+                    createDirs = subprocess.Popen("createDirsGUI.bat", cwd=r"Path\\to\\createDirsGui", stdout = subprocess.PIPE) #set up the directories
+                    #verify createDirs
+                    date = datetime.now()
+                    filename = date.strftime("Run_%Y%m%d_%H%M%S")
+                    checkFilename = glob.glob(filename)
+                    if checkFilename[0] == filename:
+                        createDirs_state = 1
             if system_state == 0: 
-                if createDirs_state == 1: #effectively runs the startCapture.bat file
-                    if !velodynePortStarted: #run startVelodynePort and verify
-                        startVelodynePort = subprocess.Popen(shlex.split("start startVelodynePortGUI lidar\port 1"), cwd=r"Path\\to\\startVelodynePortGUI", stdout = subprocess.PIPE)
-                    if !velodyneStarboardStarted: #run startVelodyneStarboard and verify
-                        startVelodyneStarboard = subprocess.Popen("start startVelodyneStarboardGUI lidar\starboard 1", cwd=r"Path\\to\\startVelodyneStarboardGUI", stdout = subprocess.PIPE)
-                    if !ladybugPortStarted: #run startLadybugPort and verify
-                        startLadybugPort = subprocess.Popen("start startLadybugPort ladybug\port 1", cwd=r"Path\\to\\startLadybugPortGUI", stdout = subprocess.PIPE)
-                    if !ladybugStarboardStarted: #run startLadybugStarboad and verify
-                        startLadybugStarboad = subprocess.Popen("start startLadybugStarboard ladybug\starboard 1", cwd=r"Path\\to\\startLadybugStarboardGUI", stdout = subprocess.PIPE)
-                    if !arduinoStarted: #run startArduino and verify
-                        startArduino = subprocess.Popen("start startArduino time_sync_files 1", cwd=r"Path\\to\\startArduinoGUI", stdout = subprocess.PIPE)
-                    if velodynePortStarted and velodyneStarboardStarted and ladybugPortStarted and ladybugStarboardStarted and arduinoStarted: #if all verification true
-                        system_state = 1
+                if createDirs_state == 1: #data for lidar, ladybug, arduino can be saved in proper folders
+                    if cmd == "startLidar":
+                        if !velodynePortStarted: #run startVelodynePort and verify
+                            startVelodynePort = subprocess.Popen(shlex.split("start startVelodynePortGUI lidar\port 1"), cwd=r"Path\\to\\startVelodynePortGUI", stdout = subprocess.PIPE)
+                            processes.append(startVelodynePort)
+                        if !velodyneStarboardStarted: #run startVelodyneStarboard and verify
+                            startVelodyneStarboard = subprocess.Popen("start startVelodyneStarboardGUI lidar\starboard 1", cwd=r"Path\\to\\startVelodyneStarboardGUI", stdout = subprocess.PIPE)
+                            processes.append(startVelodyneStarboard)
+                    if cmd == "startLadybug":
+                        if !ladybugPortStarted: #run startLadybugPort and verify
+                            startLadybugPort = subprocess.Popen("start startLadybugPortGUI ladybug\port 1", cwd=r"Path\\to\\startLadybugPortGUI", stdout = subprocess.PIPE)
+                            processes.append(startLadybugPort)
+                        if !ladybugStarboardStarted: #run startLadybugStarboad and verify
+                            startLadybugStarboard = subprocess.Popen("start startLadybugStarboardGUI ladybug\starboard 1", cwd=r"Path\\to\\startLadybugStarboardGUI", stdout = subprocess.PIPE)
+                            processes.append(startLadybugStarboard)
+                    if cmd == "startArduino":
+                        if !arduinoStarted: #run startArduino and verify
+                            startArduino = subprocess.Popen("start startArduinoGUI time_sync_files 1", cwd=r"Path\\to\\startArduinoGUI", stdout = subprocess.PIPE)
+                            processes.append(startArduino)
+
                 
         if dbCommand == stp:
             if system_state == 1:
