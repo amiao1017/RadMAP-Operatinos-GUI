c12572d5f55524518571b6941075e99fd3de1d72
Shreyas Srinivasan <Shreyas@Shreyass-MacBook-Pro-2.local>
2014-10-23 12:05:59 -0700

    updated hsServer code

--- Operations GUI/Server Code/hsServer.py
+++ Operations GUI/Server Code/hsServer.py
@@ -2,6 +2,7 @@ import zmq
 import subprocess
 
 context = zmq.Context()
+HyperSpecAcqStarted = False
 
 #  Sockets to talk to servers
 dbSocket = context.socket(zmq.REP)
@@ -11,13 +12,21 @@ while True:
 
 	dbCommand = dbSocket.recv()
 
-	if dbCommand == str:
-
-	#based on what the start script returns may want to use subprocess.callback() and use resulting info for data verification
-		subprocess.call(#insert hs start script name here#)
+	if dbCommand == 'STA':
+		if HyperSpecAcqStarted == False: #run HyperSpecAcq and verify
+            hyperSpecAcquisition = subprocess.Popen(shlex.split("2iHyperSpecAcquisitionV1.2.exe"), stdout = subprocess.PIPE, stdin = subprocess.PIPE, stderr = subprocess.STDOUT)
+            #check cwd
+            HyperSpecAcqStarted = True
+        else:
+            print "HyperSpec Acquisition already running"
 		
-	if dbCommand == stp:
-		subprocess.call(#insert hs stop script name here#)
+	if dbCommand == 'STO':
+		if HyperSpecAcqStarted: #stop lidars if started
+            startVelodynePort.communicate(input = 'q')
+            print "Stopping HyperSpec Acquisition"
+            HyperSpecAcqStarted = False
+        else:
+            print "HyperSpec Acquisition already stopped"
 	
 	if dbCommand == save:
 		subprocess.call(#insert hs save script name here#)
