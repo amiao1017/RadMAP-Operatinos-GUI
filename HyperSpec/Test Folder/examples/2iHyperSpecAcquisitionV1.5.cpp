fdc9e8b64323991728fe82dc8f41f8bdf5c8b200
Rmeyer <rmeyer@lbl.gov>
2014-10-29 11:12:58 -0700

    Update HSV1.5 fix folder and exit

--- HyperSpec/Test Folder/examples/2iHyperSpecAcquisitionV1.5.cpp
+++ HyperSpec/Test Folder/examples/2iHyperSpecAcquisitionV1.5.cpp
@@ -195,9 +195,11 @@ void makeCube(std::pair<unsigned short *,int> myData)
 		directoryName = "./";
 		directoryName.append(fileDay);
 		directoryName.append(directoryTime);
-		directoryName.append("/");
+		//directoryName.append("/");
 	}
+	CreateDirectoryA(directoryName,NULL);
 	header_filename = directoryName;
+	header_filename.append("/");
 	header_filename.append(saveName);
 	header_filename.append(".hdr");
 	std::ofstream outfile(header_filename.c_str());
@@ -225,7 +227,7 @@ void makeCube(std::pair<unsigned short *,int> myData)
 
 	//write data file
 	std::ofstream cubefile;	
-	cubeSaveName = directoryName + saveName + ".bil";		
+	cubeSaveName = directoryName + "/" + saveName + ".bil";		
 	cubefile.open(cubeSaveName.c_str(), std::ios::out | std::ios::binary);
 	//cubefile.write((const char*) buffer, cubesize * sizeof(unsigned short));
 	cubefile.write((const char*) myData.first, framesize * myData.second * sizeof(unsigned short));
@@ -273,6 +275,4 @@ void writeThread(void *)
 		myQueue.pop();
 		makeCube(myData);
 	} 
-exit(EXIT_SUCCESS);
-
 }
