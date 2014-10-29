5cf114d6f0467d4a15f3dcee9077219a95163b6c
Rmeyer <rmeyer@lbl.gov>
2014-10-29 11:32:51 -0700

    Update HSV1.5 fix folder and exit

--- HyperSpec/Test Folder/examples/2iHyperSpecAcquisitionV1.5.cpp
+++ HyperSpec/Test Folder/examples/2iHyperSpecAcquisitionV1.5.cpp
@@ -155,7 +155,7 @@ int main(int argc, char* argv[])
 		grabbingFrames = FALSE;
 		imager.disconnect(); 			//disconnect
 
-		return EXIT_SUCCESS;
+		return 0;
 		
 	} catch (std::exception const & e)
 	{
