c1d1a0a0734ab699d4076fe173451e4938ea220a
Rmeyer <rmeyer@lbl.gov>
2014-12-16 13:15:50 -0800

    TimeTest Parser

--- HyperSpec/Test Folder/bin/hsTimeTestParser.py
+++ HyperSpec/Test Folder/bin/hsTimeTestParser.py
@@ -24,5 +24,5 @@ for files in headers:
 			firstLineTime = line.split('at')[1]
 			firstLineTime = firstLineTime.split(' ')[1]
 			doc = open('%s.txt' %docName, 'a')
-			doc.write('%s, %s, %d \n' %(fileTime, firstLineTime, counter))
+			doc.write('%d, %s, %s' %(counter, fileTime, firstLineTime))
 			doc.close()
