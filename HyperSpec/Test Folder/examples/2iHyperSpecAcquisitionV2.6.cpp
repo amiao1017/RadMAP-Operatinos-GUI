732835a2573121473de38f9e9be4562873bbdb1b
Rmeyer <rmeyer@lbl.gov>
2015-01-15 12:54:47 -0800

    2i V2.6 Edit for final cube

--- HyperSpec/Test Folder/examples/2iHyperSpecAcquisitionV2.6.cpp
+++ HyperSpec/Test Folder/examples/2iHyperSpecAcquisitionV2.6.cpp
@@ -230,7 +230,7 @@ void makeCube(std::tuple<unsigned short *, int, std::string *> myData)
 	outfile << "Time Stamp Vector = { \n";
 	for(int i = 0; i < 1000; i++)
 		{
-			if (std::get<2>(myData)[i] == 0)
+			if (std::get<2>(myData)[i].empty())
 			{
 				outfile << "No Data"
 			}
