f580b73129fb7618bd5427af0dbe45bb46c92d93
Rmeyer <rmeyer@lbl.gov>
2015-01-15 13:09:09 -0800

    2i V2.6 Edit for final cube

--- HyperSpec/Test Folder/examples/2iHyperSpecAcquisitionV2.6.cpp
+++ HyperSpec/Test Folder/examples/2iHyperSpecAcquisitionV2.6.cpp
@@ -232,10 +232,11 @@ void makeCube(std::tuple<unsigned short *, int, std::string *> myData)
 		{
 			if (!std::get<2>(myData)[i].empty())
 			{
-			outfile << std::get<2>(myData)[i];
-			if (i < 999)
-			{
-				outfile << ", \n";
+				outfile << std::get<2>(myData)[i];
+				if (i < 999)
+				{
+					outfile << ", \n";
+				}
 			}
 			else outfile << "\n";
 		}
