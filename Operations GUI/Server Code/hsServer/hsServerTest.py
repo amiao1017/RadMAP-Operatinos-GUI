fa3b9203925c85f730f2e7425f36a0da7cb9e8ed
rossebv <rmeyer@lbl.gov>
2015-03-05 14:47:21 -0500

    committing hsServer

--- Operations GUI/Server Code/hsServer/hsServerTest.py
+++ Operations GUI/Server Code/hsServer/hsServerTest.py
@@ -10,41 +10,46 @@ HyperSpecAcqStarted = False
 
 #  Sockets to talk to servers
 dbSocket = context.socket(zmq.REP)
-dbSocket.connect("tcp://192.168.1.100:5108")
+dbSocket.connect("tcp://192.168.100.1:5110")
 
 while True:
-
     if dbSocket.poll(1) != 0:
-    	dbCommand = dbSocket.recv()
+        dbCommand = dbSocket.recv()
+        print dbCommand
 
-    	if dbCommand == 'STA':
-    	    if HyperSpecAcqStarted == False: #run HyperSpecAcq and verify
-                #2iAcquisition = subprocess.Popen(shlex.split("E:\ResononAPI_2.2_Beta\bin\2iHyperSpecAcquisitionV2.0.exe"), stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
-                #processes.append(2iAcquisition)
-                #NIRAcquisition = subprocess.Popen(shlex.split("E:\ResononAPI_2.2_Beta\bin\NIRHyperSpecAcquisitionV1.0.exe"), stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
-                #processes.append(NIRAcquisition)
-                print "HyperSpec Acquisition starting"
-                HyperSpecAcqStarted = True
-            else:
-                print "HyperSpec Acquisition already running"
-    		
-    	if dbCommand == 'STO':
-    	    if HyperSpecAcqStarted: #stop HyperSpec if started
-                #2iAcquisition.communicate(input = 'q')
-                #processes.remove(2iAcquisition)
-                #NIRAcquisition.communicate(input = 'q')
-                #processes.remove(NIRAcquisition)
-                print "Stopping HyperSpec Acquisition"
-                HyperSpecAcqStarted = False
-            else:
-                print "HyperSpec Acquisition already stopped"
-
-    print "Verifying Processes"	
-    #if len(processes) != 0:
-        #line = processes[iterations % len(processes)].stdout.readline() #NEED TIMEOUT TO PREVENT BLOCKING
-        #if (line.find("line") != -1) and (iterations % 2 = 0): #2i verification
-             #dbSocket.send("2i -" + line[line.find("line"): ])
-        #if (line.find("line") != -1) and (iterations % 2 = 1): #NIR Verification
-            #dbSocket.send("NIR -" + line[line.find("line"): ])
-    iterations += 1
+##while True:
+##
+##    if dbSocket.poll(1) != 0:
+##    	dbCommand = dbSocket.recv()
+##
+##    	if dbCommand == 'STA':
+##    	    if HyperSpecAcqStarted == False: #run HyperSpecAcq and verify
+##                #2iAcquisition = subprocess.Popen(shlex.split("E:\ResononAPI_2.2_Beta\bin\2iHyperSpecAcquisitionV2.0.exe"), stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+##                #processes.append(2iAcquisition)
+##                #NIRAcquisition = subprocess.Popen(shlex.split("E:\ResononAPI_2.2_Beta\bin\NIRHyperSpecAcquisitionV1.0.exe"), stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+##                #processes.append(NIRAcquisition)
+##                print "HyperSpec Acquisition starting"
+##                HyperSpecAcqStarted = True
+##            else:
+##                print "HyperSpec Acquisition already running"
+##    		
+##    	if dbCommand == 'STO':
+##    	    if HyperSpecAcqStarted: #stop HyperSpec if started
+##                #2iAcquisition.communicate(input = 'q')
+##                #processes.remove(2iAcquisition)
+##                #NIRAcquisition.communicate(input = 'q')
+##                #processes.remove(NIRAcquisition)
+##                print "Stopping HyperSpec Acquisition"
+##                HyperSpecAcqStarted = False
+##            else:
+##                print "HyperSpec Acquisition already stopped"
+##
+##    print "Verifying Processes"	
+##    #if len(processes) != 0:
+##        #line = processes[iterations % len(processes)].stdout.readline() #NEED TIMEOUT TO PREVENT BLOCKING
+##        #if (line.find("line") != -1) and (iterations % 2 = 0): #2i verification
+##             #dbSocket.send("2i -" + line[line.find("line"): ])
+##        #if (line.find("line") != -1) and (iterations % 2 = 1): #NIR Verification
+##            #dbSocket.send("NIR -" + line[line.find("line"): ])
+##    iterations += 1
 	
---
+++ Operations GUI/Server Code/hsServer/hsServerTestV1.1.py
@@ -0,0 +1,64 @@
+import zmq
+import subprocess
+import shlex
+import time
+import sys
+
+context = zmq.Context()
+port = "5556"
+dbSocket = context.socket(zmq.PAIR)
+dbSocket.bind("tcp://192.168.100.43:%s" % port)
+
+processes = [] #list of processes
+iterations = 0
+HyperSpecAcqStarted = False
+
+while True:
+
+    if dbSocket.poll(100) != 0:
+    	dbCommand = dbSocket.recv()
+        print dbCommand
+
+    	if dbCommand == 'startHyperSpec':
+            print "Command Received - %s" % dbCommand
+    	    #if HyperSpecAcqStarted == False: #run HyperSpecAcq and verify
+                #testAcquitision = subprocess.call(shlex.split("python /home/rossebv/Desktop/RadMAP-Operatinos-GUI/Operations\ GUI/interfaceCodeV0.2.py &"))
+            print "Acquisition Script Called"
+                #2iAcquisition = subprocess.Popen(shlex.split("E:\ResononAPI_2.2_Beta\bin\2iHyperSpecAcquisitionV2.0.exe"), stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+                #processes.append(2iAcquisition)
+                #NIRAcquisition = subprocess.Popen(shlex.split("E:\ResononAPI_2.2_Beta\bin\NIRHyperSpecAcquisitionV1.0.exe"), stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+                #processes.append(NIRAcquisition)
+            print "HyperSpec Acquisition starting"
+            
+                #HyperSpecAcqStarted = True
+            #else:
+                #print "HyperSpec Acquisition already running"
+
+        # if dbCommand == 'startHyperSpec':
+        #     print "dbCommand = startHyperSpec"
+        #     if HyperSpecAcqStarted == False: #run HyperSpecAcq and verify
+        #         testAcquitision = subprocess.call(shlex.split("python /home/rossebv/Desktop/RadMAP-Operatinos-GUI/Operations\ GUI/interfaceCodeV0.2.py &"))
+        #         print "Acquisition Script Called"
+
+    		
+    	if dbCommand == 'stopHyperSpec':
+    	    if HyperSpecAcqStarted: #stop HyperSpec if started
+                #2iAcquisition.communicate(input = 'q')
+                #processes.remove(2iAcquisition)
+                #NIRAcquisition.communicate(input = 'q')
+                #processes.remove(NIRAcquisition)
+                print "Stopping HyperSpec Acquisition"
+                HyperSpecAcqStarted = False
+            else:
+                print "HyperSpec Acquisition already stopped"
+        
+
+    #print "Verifying Processes"	
+    #if len(processes) != 0:
+        #line = processes[iterations % len(processes)].stdout.readline() #NEED TIMEOUT TO PREVENT BLOCKING
+        #if (line.find("line") != -1) and (iterations % 2 = 0): #2i verification
+             #dbSocket.send("2i -" + line[line.find("line"): ])
+        #if (line.find("line") != -1) and (iterations % 2 = 1): #NIR Verification
+            #dbSocket.send("NIR -" + line[line.find("line"): ])
+    iterations += 1
+	
